bb287071ea36803cc7b761b451702428d2eeb5d5
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-12-04 15:38:39 +0300

    fix: Fixed bug with showing searchBar after transition from FavouritesViewController

--- NewsApp/News/NewsViewController.swift
+++ NewsApp/News/NewsViewController.swift
@@ -42,15 +42,11 @@ final class NewsViewController: UIViewController {
         configureUI()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        
-    }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         navigationController?.delegate = self
+        
+        configureSearchBar()
     }
 }
 
